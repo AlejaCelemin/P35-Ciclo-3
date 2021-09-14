88680737daecc285ee060e5f59a6ce4305244ce9
AlejaCelemin <alejaceleminmartinez@gmail.com>
2021-09-14 18:57:02 -0500

    Agregado tarea 1 LogIn

---
+++ Avance_00.py
@@ -0,0 +1,5 @@
+###.   MÓDULO DE LOGIN DEL SISTEMA
+#EL EQUIPO deberá desarrollar el módulo que permita el ingreso al sistema, una vez se haya realizado la validación por nombre de usuario y contraseña.
+### Deberá tenerse un usuario por defecto con el nombre de admininicial, y contraseña admin123456 para su ingreso la primera vez.
+
+############  AVANCE_00 ################
