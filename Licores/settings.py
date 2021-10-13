df5bb7c53b5907ed5c880b18a58b56d29612a641
AlejandraCelemin <alejaceleminmartinez@gmail.com>
2021-10-12 19:22:10 -0500

    modelo vista controlador

--- Licores/settings.py
+++ Licores/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'Licores.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'licores_donmintic',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
