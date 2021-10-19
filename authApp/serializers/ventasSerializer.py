ce81157665665534b84c81d56ab740664e3302a3
AlejandraCelemin <alejaceleminmartinez@gmail.com>
2021-10-19 15:08:04 -0500

    deployment changes

--- authApp/serializers/ventasSerializer.py
+++ authApp/serializers/ventasSerializer.py
@@ -7,16 +7,21 @@ from rest_framework          import serializers
 class VentasSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Ventas
-        fields = ['quantity', 'fecha']
+        fields = ['quantity', 'fecha','productos']
 
     def to_representation(self, obj):
+        productos = Productos.objects.get(id=obj.productos)
         ventas    = Ventas.objects.get(id=obj.id)
 
         return {
 
             "id": ventas.id,
             "quantity": ventas.quantity,
-            "fecha": ventas.fecha
+            "fecha": ventas.fecha,
 
-
+            'productos': {
+                 "id_producto": productos.id_producto,
+                 "description":productos.description,
+                 "price":productos.price
+            }
         }
