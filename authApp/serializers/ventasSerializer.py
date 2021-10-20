27443ad4cf2aad75f6f6cab2463e1887de0e4947
AlejandraCelemin <alejaceleminmartinez@gmail.com>
2021-10-20 16:19:16 -0500

    deployment changes

--- authApp/serializers/ventasSerializer.py
+++ authApp/serializers/ventasSerializer.py
@@ -1,34 +1,39 @@
 from django.db.models import fields
 from authApp.models import productos
+from authApp.models import user
 from authApp.models.productos import Productos
 from authApp.models.ventas import Ventas
 from rest_framework          import serializers
+from authApp.models.user import User
 
 class VentasSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Ventas
-        fields = ['quantity', 'fecha','productos']
+        fields = ['id','quantity', 'fecha','user','productos']
 
     def create(self, validated_data):
         productos_data = validated_data.pop('productos')
+        user_data = validated_data.pop('user')
         ventainstancia = Ventas.objects.create(**validated_data)
         Productos.objects.create(ventas = ventainstancia,**productos_data)
+        User.objects.create(ventas = ventainstancia, **user_data)
 
         return ventainstancia
 
     def to_representation(self, obj):
         productos = Productos.objects.get(id=obj.productos)
         ventas    = Ventas.objects.get(id=obj.id)
+        user      = User.objects.get(id=obj.id)
 
         return {
 
+            
             "id": ventas.id,
             "quantity": ventas.quantity,
             "fecha": ventas.fecha,
+            "user": user.id,
+            "producto": productos.id_producto
+                 
 
-            "productos": {
-                 "id_producto": productos.id_producto,
-                 "description":productos.description,
-                 "price":productos.price
-            }
+            
         }
