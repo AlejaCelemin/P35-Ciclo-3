c52d682b49d28f4a9fc0cf93a22da6040c62a50a
AlejandraCelemin <alejaceleminmartinez@gmail.com>
2021-10-19 22:30:39 -0500

    deployment changes

--- authApp/views/viewsproductos.py
+++ authApp/views/viewsproductos.py
@@ -29,20 +29,13 @@ class ProductosCreateView(generics.CreateAPIView):
 
 class ProductosDeleteView(generics.DestroyAPIView):
     serializer_class =ProductosSerializer
-    permission_classes =(IsAuthenticated,)
     queryset = Productos.objects.all()
 
     def get(self, request, *args, **kwargs):
         print("Request",request)
         print("Args", args)
         print("KWArgs", kwargs)
-        token = request.META.get('HTTP_AUTHORIZATION')[7:]
-        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
-        valid_data = tokenBackend.decode(token,verify=False)
 
-        if valid_data['user_id'] != kwargs['user']:
-            stringResponse = {'detail':'Unauthorized Request'}
-            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED) 
 
         return super().destroy(request, *args, **kwargs)
     
