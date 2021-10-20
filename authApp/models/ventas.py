2ab0e4056c09f57d23746f16751980df3f3e040a
AlejandraCelemin <alejaceleminmartinez@gmail.com>
2021-10-20 18:01:12 -0500

    modelo ventas

--- authApp/models/ventas.py
+++ authApp/models/ventas.py
@@ -6,7 +6,7 @@ class Ventas (models.Model):
     id = models.AutoField(primary_key=True)
     quantity = models.IntegerField(default=0)
     fecha = models.DateTimeField()
-    user = models.ForeignKey(User, related_name='ventas', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
     productos = models.ForeignKey(Productos, related_name='productos', on_delete=models.CASCADE)
     
 
